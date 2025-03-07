437f66420868dbcbb520e2d752ff87af916c20f8
justojaramillo <justojaramillo1991@gmail.com>
2025-03-06 22:36:43 -0500

    Traits practice

--- traits/trait.php
+++ traits/trait.php
@@ -7,17 +7,28 @@ trait Display{
         echo $message;
     }
 }
+trait SaveData{
+    protected function saveThis($message){
+        return $message;
+    }
+}
 
 
 class Main{
 
     use  Display;
+    use SaveData;
 
     public function displayingDate(){
         $this->showThis('Trait used');
     }
+
+    public function savingData(){
+        return $this->saveThis('Saving Data');
+    }
 }
 (new Main())->displayingDate(); # parentheses not requiered since PHP 8
 new Main()->displayingDate();
+echo (new Main()->savingData());
 
 ?>
